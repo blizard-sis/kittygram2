b99ff673f216b56b7149d0206f966be5391481e2
Ilya Seleznev <blizardsis@yandex.ru>
2023-02-01 17:27:51 +0300

    1

--- cats/models.py
+++ cats/models.py
@@ -25,10 +25,21 @@ class Cat(models.Model):
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
         User, related_name='cats', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+    achievements = models.ManyToManyField(
+        Achievement,
+        through='AchievementCat'
+    )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owner'],
+                name='unique_name_owner'
+            )
+        ]
 
 
 class AchievementCat(models.Model):
